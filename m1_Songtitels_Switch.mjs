ac1f1d680db3cdcedb118d2c5f71af669bad152e
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-09-29 19:37:55 +0200

    update main
    verkorte versie door console.log 1 keer onderaan te schrijven

--- m1_Songtitels_Switch.mjs
+++ m1_Songtitels_Switch.mjs
@@ -2,6 +2,47 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
+
+console.log('Kies een artiest:');
+console.log('1. Armin van Buuren');
+console.log('2. Tiësto');
+console.log('3. Charlotte de Witte');
+console.log('4. Steve Aoki');
+
+let keuze = parseFloat(await userInput.question('Geef je keuze in: '));
+let artiest, nummer;
+
+switch (keuze){
+    case 1:
+        artiest = 'Armin van Buuren';
+        nummer = 'On & On';
+        break;
+    case 2:
+        artiest = 'Tiësto';
+        nummer = 'Drifting';
+        break;
+    case 3: 
+        artiest = 'Charlotte de Witte';
+        nummer = 'Age Of Love';
+        break;
+    case 4:
+        artiest = 'Steve Aoki';
+        nummer = 'Pursuit of Happiness';
+        break;
+    default: 
+        console.log('Je koos voor ' + keuze + ' maar dit is geen geldige keuze.');
+        process.exit();
+}
+
+console.log('je koos voor ' + artiest + '.');
+console.log('Een nummer van deze artiest is: ' + nummer + '.');
+
+
+process.exit();
+
+
+
+/*
 let nummerArtiest1 = 'Een nummer van deze artiest is: "On & On"';
 let nummerArtiest2 = 'Een nummer van deze artiest is: "Drifting"';
 let nummerArtiest3 = 'Een nummer van deze artiest is: "Age Of Love"';
@@ -34,6 +75,4 @@ switch (keuze){
         break;
     default: 
     console.log('Je koos voor "' + keuze + '" maar dit is geen keuze.');
-}
-
-process.exit();
+}*/
