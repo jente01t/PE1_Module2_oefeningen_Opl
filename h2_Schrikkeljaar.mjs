3f717531a97a8265f27d9521633dc5912652a4fe
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-09-30 11:47:39 +0200

    update main

---
+++ h2_Schrikkeljaar.mjs
@@ -0,0 +1,15 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+import { parse } from 'node:url';
+const userInput = readline.createInterface({input, output});
+
+let jaar = parseFloat(await userInput.question('Geef een jaar: '));
+
+
+if ((jaar % 4 == 0) && (jaar % 100 != 0) || (jaar % 400 == 0) ) {
+    console.log(jaar + ' is een schrikkeljaar.');
+} else {
+    console.log(jaar + ' is geen schrikeljaar');
+}
+
+process.exit();
